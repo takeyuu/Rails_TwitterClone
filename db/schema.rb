50ae874b8b05f34b8ae3f72c1ecfc16883f8fbf7
takeoka <tokih4ru@gmail.com>
2020-07-26 17:01:13 +0900

    postモデル作成

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2020_07_26_075226) do
+
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
