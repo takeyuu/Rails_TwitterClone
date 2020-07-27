a3f8d6430b88f657e6a0f7ad6327d468d117d037
takeoka <tokih4ru@gmail.com>
2020-07-27 09:10:45 +0900

    新規投稿機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: 'つぶやきました'
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_posts_path
+    # elsif action_name == 'edit'
+    end
+  end
 end
